6e7dc30b1ca9bf21ea1223a4065f21675859bacb
Masato2524 <crest.muc0424@gmail.com>
2020-10-19 16:50:04 +0900

    complete

--- 2_Rayleigh/Rayleigh.py
+++ 2_Rayleigh/Rayleigh.py
@@ -1,14 +1,42 @@
 import numpy as np
-from scipy import signal
 import matplotlib.pyplot as plt
-import matplotlib
-#del matplotlib.font_manager.weight_dict['roman']
-#matplotlib.font_manager._rebuild()
-import os
-import pandas as pd
-workdir = "C:/Users/fs-agent/Desktop/JAXA進捗/20201014/plate_2d/t0.00417"
 import time
 start = time.time()
 
-#aa
+eta=2.0
+sep=4
+dtaw=0.05
+deta=eta/sep
+ita=100
+
+def rayleigh():
+
+    for n in range(ita):
+        for k in range(2,sep+1):
+            U[k]=U[k]+dtaw*((U[k+1]-2*U[k]+U[k-1])/(deta)**2+(k-1)*(U[k+1]-U[k-1]))
+    return U
+
+
+
 if __name__=='__main__':
+    U=np.zeros(sep+2)
+    U[1]=1
+    U[-1]=0
+    rayleigh()
+    etag=np.zeros(sep+2)
+    for i in range(1,sep+2):
+        etag[i]=(i-1)*deta
+
+    print(U)
+    print(etag)
+    plt.rcParams['font.family'] ='Times New Roman'
+    plt.rcParams['mathtext.fontset'] = 'stix'
+    plt.figure()
+    plt.subplot(111)
+    plt.xlabel("U")
+    plt.ylabel('$\eta$')
+    plt.plot(U[1:sep+2], etag[1:sep+2],"k",marker='o')
+    plt.show()
+
+    elapsed_time = time.time() - start
+    print("elapsed_time:{0}".format(elapsed_time))
